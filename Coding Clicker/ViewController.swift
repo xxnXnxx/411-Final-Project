c1473e4ecc952b64a05bda12b3023a8626660bbd
braddeboer <143136043+braddeboer@users.noreply.github.com>
2024-12-05 15:51:10 -0800

    Add files via upload

--- Coding Clicker/ViewController.swift
+++ Coding Clicker/ViewController.swift
@@ -14,9 +14,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        updateCostLabel(label: Upgrade_1_Cost, price: upgrade1Cost)
-        fadeInAndOut(label: CannotAffordUpgradeLabel)
         
+        //updates each exp label to 0 on startup
+        updateExpCount(label: Exp_Count_Display)
+        updateExpCountInUpgradeMenu(label: Upgrade_Menu_Exp)
+        
+        //updates upgrade 1 cost label in menu
+        updateCostLabel(label: Upgrade_1_Cost, price: upgrade1Cost)
+        //updates upgrade 1 quantity label in menu
+        updateQuantityLabelUpgrade1(label: Upgrade1Quantity, quantity: upgrade1quantity)
+        
+        //updates upgrade 2 cost label in menu
+        updateCostLabel(label: Upgrade2Cost, price: upgrade2cost)
+        //updates upgrade 2 quantity label in menu
+        updateQuantityLabelUpgrade2(label: Upgrade2Quantity, quantity: Int(upgrade2quantity))
+        
+        
+        
+        //hides the cannot afford upgrade label
+        fadeInAndOut(label: CannotAffordUpgradeLabel)
+        //hides max amount of upgrade label
+        fadeInAndOut(label: MaxAmountReached)
+        
+        
+        //flip top triangles to make background for exp display
         expDisplay1.transform = CGAffineTransform(scaleX: 1, y: -1)
         expDisplay2.transform = CGAffineTransform(scaleX:1, y: -1)
     }
@@ -34,27 +55,27 @@ class ViewController: UIViewController {
         updateExpCount(label: Exp_Count_Display)
         
         //if chain will change the background color at certain intervals of exp
-        if exp == 10{
+        if exp >= 100{
             view.backgroundColor = .yellow
             Upgrade_Menu_Ui_View.backgroundColor = .yellow
         }
-        if exp == 100000{
+        if exp >= 100000{
             view.backgroundColor = .blue
             Upgrade_Menu_Ui_View.backgroundColor = .blue
         }
-        if exp == 20000{
+        if exp >= 20000{
             view.backgroundColor = .green
             Upgrade_Menu_Ui_View.backgroundColor = .green
         }
-        if exp == 500000{
+        if exp >= 500000{
             view.backgroundColor = .black
             Upgrade_Menu_Ui_View.backgroundColor = .black
         }
-        if exp == 1000000{
+        if exp >= 1000000{
             view.backgroundColor = .red
             Upgrade_Menu_Ui_View.backgroundColor = .red
         }
-        if exp == 1500000{
+        if exp >= 1500000{
             view.backgroundColor = .blue
             Upgrade_Menu_Ui_View.backgroundColor = .blue
         }
@@ -64,9 +85,10 @@ class ViewController: UIViewController {
     
     
     
+    //uiLabel for when max amount of upgrade has been reached
+    @IBOutlet weak var MaxAmountReached: UILabel!
     
-    
-    
+    //uiLabel for when the user cannot afford the upgrade
     @IBOutlet weak var CannotAffordUpgradeLabel: UILabel!
     
     
@@ -91,22 +113,32 @@ class ViewController: UIViewController {
         Upgrade_Menu_Ui_View.isHidden.toggle()
         Upgrade_Menu_Exp.isHidden.toggle()
 //        CannotAffordUpgradeLabel.isHidden.toggle()
-        updateExpCount(label: Upgrade_Menu_Exp)
+        updateExpCountInUpgradeMenu(label: Upgrade_Menu_Exp)
     }
     
     
     //first upgrade available, upgrades words per minute ie increases exp multiplier
     var upgrade1multiplier = 1.0
     var upgrade1Cost = 10.0
+    var upgrade1quantity = 0
+    
+    //first upgrade quantity label
+    @IBOutlet weak var Upgrade1Quantity: UILabel!
     
     @IBAction func Upgrade1WPMBoost(_ sender: Any) {
-//        upgrade1Cost = upgrade1Cost * upgrade1multiplier
-        if (Int(exp) >= Int(upgrade1Cost)) {
+
+        //flashes the maxamountreach label on screen when user has purchased upgrade 1 10 times
+        if (upgrade1quantity == 10) {
+            fadeInAndOut(label: MaxAmountReached)
+        }
+        else if (Int(exp) >= Int(upgrade1Cost) && upgrade1quantity < 10) {
             exp -= upgrade1Cost
-            
+            upgrade1quantity += 1
+                
             //updates all necessary information after purhcasing the upgrade
             updateExpCountInUpgradeMenu(label: Upgrade_Menu_Exp)
             updateExpCount(label: Exp_Count_Display)
+            updateQuantityLabelUpgrade1(label: Upgrade1Quantity, quantity: upgrade1quantity)
             
             multiplier += 0.25
             upgrade1multiplier *= 1.23
@@ -120,6 +152,47 @@ class ViewController: UIViewController {
     }
     
     
+    //upgrade 2 (intern upgrade) member variables
+    var upgrade2quantity = 0.0
+    var upgrade2cost = 100.0
+    var ownsUpgrade2 = false
+    
+   
+    @IBOutlet weak var Upgrade2Cost: UILabel!
+    
+    @IBOutlet weak var Upgrade2Quantity: UILabel!
+    
+    @IBAction func HireInternsUpgrade(_ sender: Any) {
+        if (upgrade2quantity == 5) {
+            fadeInAndOut(label: MaxAmountReached)
+        }
+        else if (Int(exp) >= Int(upgrade2cost) && upgrade2quantity < 5) {
+            exp -= upgrade2cost
+            upgrade2quantity += 1
+            ownsUpgrade2 = true
+            upgrade2cost += 100
+            //updates cost in menu
+            updateCostLabel(label: Upgrade2Cost, price: upgrade2cost)
+            
+            //updates all necessary information after purhcasing the upgrade
+            updateExpCountInUpgradeMenu(label: Upgrade_Menu_Exp)
+            updateExpCount(label: Exp_Count_Display)
+            updateQuantityLabelUpgrade2(label: Upgrade2Quantity, quantity: Int(upgrade2quantity))
+            
+            Task {
+                while ownsUpgrade2 {
+                    try await Task.sleep(nanoseconds: 2_000_000_000)  // 2 seconds
+                    exp += 1 * upgrade2quantity
+                    updateExpCountInUpgradeMenu(label: Upgrade_Menu_Exp)
+                    updateExpCount(label: Exp_Count_Display)
+                }
+            }
+        }
+        else if (Int(exp) < Int(upgrade2cost)){
+            fadeInAndOut(label: CannotAffordUpgradeLabel)
+        }
+    }
+    
     
     
     
@@ -131,17 +204,30 @@ class ViewController: UIViewController {
     
     //This function will update the text that is on the ui label whenever the button is clicked
     func updateExpCount(label: UILabel) {
-        label.text = "Taps: \(Int(exp))"
+        label.text = "\(Int(exp))"
         label.textAlignment = .center
     }
-    //this function functions the same just without the center alignment of the text
+//this function functions the same just without the center alignment of the text
     func updateExpCountInUpgradeMenu(label: UILabel) {
-        label.text = "Taps: \(Int(exp))"
+        label.text = "Spendable Currency: \(Int(exp))"
+        label.textAlignment = .left
     }
     
     //this function will update each cost label with the new associated cost for each upgrade
     func updateCostLabel (label: UILabel, price: Double) {
         label.text = "\(Int(price))"
+        label.textAlignment = .center
+    }
+    
+    //this function updates the quantity labels for upgrade 1 (wpm)
+    func updateQuantityLabelUpgrade1 (label: UILabel, quantity: Int) {
+        label.text = "\(quantity)/10"
+        label.textAlignment = .center
+    }
+    //this function updates the quantity label for upgrade 2 (interns)
+    func updateQuantityLabelUpgrade2 (label: UILabel, quantity: Int) {
+        label.text = "\(quantity)/5"
+        label.textAlignment = .center
     }
 
     //allows a UILabel to fade in and out of the scene
